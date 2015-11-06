9e2425330a2539f68ea0f2ed9c7a4afb26845bfe
pete franco 4 <petethe4th@live.com>
2015-11-05 19:27:22 -0600

    initial commit release1.0.0

---
+++ MyFirstWebsite/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MyFirstWebsite.Models;
+
+namespace MyFirstWebsite.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Sum(int a, int b, string name)
+        {
+            var result = a + b;
+            
+            NumberModel numberModel = new NumberModel();
+            numberModel.MyNumber = result;
+            numberModel.Name = name;
+
+            return View(numberModel); 
+        }
+
+    }
+}
---
+++ MyFirstWebsite/Models/NumberModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyFirstWebsite.Models
+{
+    public class NumberModel
+    {
+        public int MyNumber { get; set; }
+        public string Name { get; set; }
+    }
+}
